2f8e7e78ec921118c8cc9afa219cfaaf8809d087
Shrooq <Shrooq@DESKTOP-UU9TB91>
2018-02-12 15:28:19 -0800

    Hello

---
+++ src/Test.java
@@ -0,0 +1,44 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import static org.junit.Assert.*;
+
+
+class Test {
+
+	Model l = new Model();
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@org.junit.jupiter.api.Test
+	void test() {
+		//fail("Not yet implemented");
+		
+		assertEquals((int)2.0,(int)l.Div(8.0,4.0));
+		assertEquals(1,(int)(l.Div(8.0,0.0)));
+		assertEquals(1,(int)(l.mod(8.0,0.0)));
+		assertEquals(1,(int)(l.sqr(-1)));
+		
+		
+		
+	}
+
+	
+
+}
